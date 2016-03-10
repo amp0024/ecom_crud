1069e6ac6fb05d67e8bb4623e12073ff31c26d70
amp0024 <amp0024@gmail.com>
2016-03-10 16:12:16 -0700

    frontend work and start of routes

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -5,14 +5,40 @@ var prodQueries = require('../db/product_queries');
 var manuQueries = require('../db/manufacturers_queries');
 
 
+// router.get('/', function(req, res, next) {
+//     manuQueries.getManufacturers().then(function(mfc){
+//       res.render('index', { manufacturers: mfc });
+//   });
+// });
+
 router.get('/', function(req, res, next) {
   prodQueries.getProducts().then(function(products){
-    manuQueries.getManufacturers().then(function(mfc){
-      console.log(mfc);
-      console.log(products);
-      res.render('index', { products: products, manufacturers: mfc });
-    });
+      res.render('index', { products: products });
   });
 });
 
+
+
+// router.get('/avery', function(req, res, next) {
+//   prodQueries.getProducts().then(function(products){
+//   	var avery = filterManufacture(products, 'avery')
+//     console.log('list of man products: ', avery);
+//       res.render('index', { products: avery });
+  // });
+// });
+
+// router.get('/odell', function(req, res, next) {
+//   prodQueries.getProducts().then(function(products){
+//   	var odell = filterManufacture(products, 'odell')
+//     console.log('list of man products: ', odell);
+//       res.render('index', { products: odell });
+//   });
+// });
 module.exports = router;
+
+
+// function filterManufacture(products, name){
+// 	return products.filter(function(el){
+// 		return (el.name).toLowerCase() === name
+// 	});
+// };
--- src/server/routes/products.js
+++ src/server/routes/products.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var query = require('../db/product_queries');
+var manquery = require('../db/manufacturers_queries');
 
 router.get('/', function(req, res, next) {
   query.getProducts().then(function(products){
@@ -10,10 +11,13 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/:product_id', function(req, res, next){
-  query.getProduct(req.params.product_id).then(function(product){
-    res.json(product)
+    query.getProduct(req.params.product_id).then(function(product){
+    console.log(product)
+    res.render('productView', { products: product });
   })
 })
+
+
 
 router.post('/', function(req, res, next){
   query.createProduct(req.body).then(function(data){
@@ -30,3 +34,10 @@ router.delete('/:product_id', function(req, res, next){
 
 
 module.exports = router;
+
+
+function filterManufacture(products, name){
+  return products.filter(function(el){
+    return (el.name).toLowerCase() === name
+  });
+};

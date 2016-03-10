3b585d18eb5d89577919f20e03373b7f8970e8a8
Danny Robinson <daniel@djrobinson.me>
2016-03-10 10:09:26 -0700

    First rout for index.js

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -2,12 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 var prodQueries = require('../db/product_queries');
+var manuQueries = require('../db/manufacturers_queries');
 
 
 router.get('/', function(req, res, next) {
   prodQueries.getProducts().then(function(products){
-    res.render('index', { products: products });
-  })
+    manuQueries.getManufacturers().then(function(mfc){
+      console.log(mfc);
+      console.log(products);
+      res.render('index', { products: products, manufacturers: mfc });
+    });
+  });
 });
 
 module.exports = router;

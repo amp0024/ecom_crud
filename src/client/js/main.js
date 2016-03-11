f1f9994a7eb72783ffc3ec42722058ff4429750f
Danny Robinson <daniel@djrobinson.me>
2016-03-10 17:04:26 -0700

    Added session storage for cart, pulls data on cart page

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -3,3 +3,29 @@
 $(document).on('ready', function() {
   console.log('sanity check!');
 });
+
+var prodArr = [];
+
+$('.purchaseProduct').on('click', function(){
+  if (sessionStorage.products){
+    prodArr = JSON.parse(sessionStorage.products);
+  }
+  var product = $(this).attr('id');
+  prodArr.push(product);
+  console.log(prodArr);
+  var addVal = JSON.stringify(prodArr);
+  sessionStorage.setItem('products', addVal);
+});
+
+
+$.ajax('/api/products').done(function(data){
+  console.log(data);
+  var seshCart = JSON.parse(sessionStorage.products);
+  seshCart.map(function(id){
+    $('#cartTable').append('<h3>'+data[JSON.parse(id)-1].name+'</h3>');
+  });
+  console.log(seshCart);
+
+  });
+
+
--- src/server/migrations/20160307161140_manufacturers.js
+++ src/server/migrations/20160307161140_manufacturers.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('manufacturers', function(table){
     table.increments();
-    table.string('name');
+    table.string('mfc_name');
     table.string('location');
     table.string('contact');
     table.string('logo_url');
